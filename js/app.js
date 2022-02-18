3b40bc632c4c7b1e5586555bcc827b5e910d4abe
Marcelo Gomes <mgomes@marlin.com.br>
2022-02-17 21:10:52 -0300

    Ajustes em andamento

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-$(window).on('load', function() {
+$(window).on('load', function () {
     if ($('.preloader').length) {
         $('.preloader').fadeOut('slow');
     }
 });
 
-$('.scrollLink').click(function() {
+$('.scrollLink').click(function () {
     var href = $(this).attr('href');
     var offSetTop = $(href).offset().top - 86;
-    
+
     $(".Header").removeClass("MenuAtivo");
 
     $('html,body').animate({ 'scrollTop': offSetTop }, 1200, 'easeInOutExpo');
     return false;
-    
+
 });
 
 $(window).scroll(function () {
-var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop();
 
-if (scroll >= 2) {
-    $(".Header").addClass("BodyMove");
-} else {
-    $(".Header").removeClass("BodyMove");
-}
+    if (scroll >= 2) {
+        $(".Header").addClass("BodyMove");
+    } else {
+        $(".Header").removeClass("BodyMove");
+    }
 });
 
-$('.Container_Menu_Icone').click(function () {            
+$('.Container_Menu_Icone').click(function () {
     $(".Header").toggleClass("MenuAtivo");
 });
 
@@ -43,47 +43,47 @@ $('.NossosEmpreendimentos_Container_Cards').slick({
     nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
     dots: true,
     responsive: [{
-            breakpoint: 30000,
-            settings: {
-                slidesToScroll: 5,
-                slidesToShow: 5,
-            }
-        },
-        {
-            breakpoint: 1300,
-            settings: {
-                slidesToScroll: 4,
-                slidesToShow: 4,
-            }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 960,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 640,
-            settings: {
-                slidesToScroll: 1,
-                slidesToShow: 1,
-            }
+        breakpoint: 30000,
+        settings: {
+            slidesToScroll: 5,
+            slidesToShow: 5,
         }
+    },
+    {
+        breakpoint: 1300,
+        settings: {
+            slidesToScroll: 4,
+            slidesToShow: 4,
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToScroll: 3,
+            slidesToShow: 3,
+        }
+    },
+    {
+        breakpoint: 960,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 640,
+        settings: {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+        }
+    }
     ]
 });
 
@@ -103,9 +103,9 @@ if (window.matchMedia("(max-width: 640px)").matches) {
         nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
         dots: false
     });
-  } else {}
+} else { }
 
-  $('.CasesSucesso_Container_CasesBox').slick({
+$('.CasesSucesso_Container_CasesBox').slick({
     initialSlide: 0,
     slidesToShow: 2,
     slidesToScroll: 2,
@@ -117,46 +117,52 @@ if (window.matchMedia("(max-width: 640px)").matches) {
     nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
     dots: false,
     responsive: [{
-            breakpoint: 30000,
-            settings: {
-                slidesToScroll: 1,
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 1300,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 960,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 640,
-            settings: {
-                slidesToScroll: 1,
-                slidesToShow: 1,
-            }
+        breakpoint: 30000,
+        settings: {
+            slidesToScroll: 1,
+            slidesToShow: 3,
         }
+    },
+    {
+        breakpoint: 1300,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 1024,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 960,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToScroll: 2,
+            slidesToShow: 2,
+        }
+    },
+    {
+        breakpoint: 640,
+        settings: {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+        }
+    }
     ]
+});
+
+/* FAQ */
+$('.Container_Lista_Item').click(function () {
+    var FAQPergunta = $(this);
+    FAQPergunta.toggleClass("__is-active");
 });
